099e4ced225ec2f8db84bee2e8673aad611a912b
sharmapragya8 <sharma.pragya8@gmail.com>
2018-03-24 14:59:27 +0530

    Update palingrom.java

--- src/main/java/com/Interview/JavaPrograms/palingrom.java
+++ src/main/java/com/Interview/JavaPrograms/palingrom.java
@@ -4,6 +4,8 @@ public class palingrom {
 
 	public static void main(String[] args) {
 		System.out.println(" palindrom program");
+		
+		System.out.println(" palindrom program now modified inside the git");
 
 	}
 
